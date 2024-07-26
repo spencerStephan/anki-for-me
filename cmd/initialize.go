b6f762d2dada1c813cab801069e9545a5495d30a
Stephan Piotrowicz <stephan@vellen.ca>
2024-07-26 15:30:33 -0500

    created shell for root command

---
+++ cmd/initialize.go
@@ -0,0 +1,16 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+func init() {
+	rootCmd.AddCommand(initializeCmd)
+}
+
+var initializeCmd = &cobra.Command{
+	Use:   "init",
+	Short: "Set up Anki-for-me to work on your system",
+	Long:  "Enable your database and set up your user defaults for Anki-for-me to work properly",
+	Run: func(cmd *cobra.Command, args []string) {
+		//
+	},
+}
---
+++ cmd/root.go
@@ -0,0 +1,45 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var rootCmd = &cobra.Command{
+	Use:     "afm",
+	Version: "0.0.1",
+	Short:   "A CLI tool for memorizing things you've learned.",
+	Long:    "An interactive TUI that helps you learn by using techniques such as spaced repetition.",
+	Run:     func(cmd *cobra.Command, args []string) {},
+}
+
+var cfgFile string
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.config/anki-for-me/config.yaml")
+	rootCmd.PersistentFlags()
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+
+		viper.SetConfigType("yaml")
+		viper.SetConfigName("config")
+		viper.AddConfigPath("$HOME/.config/anki-for-me")
+
+		err := viper.ReadInConfig() // Find and read the config file
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	viper.AutomaticEnv()
+}
